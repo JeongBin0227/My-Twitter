9f1d6d63d4916fc80fcfdb872d799da44d44d484
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-10-03 01:48:01 +0900

    feat: firebase setting

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDqBYb2dIfEf2e1TTMGTgWw9DnJLCpSl2E",
+  authDomain: "nwitter-d0a11.firebaseapp.com",
+  databaseURL: "https://nwitter-d0a11.firebaseio.com",
+  projectId: "nwitter-d0a11",
+  storageBucket: "nwitter-d0a11.appspot.com",
+  messagingSenderId: "695605576109",
+  appId: "1:695605576109:web:f0f26387612f4d39e1a124",
+}
+
+export default firebase.initializeApp(firebaseConfig)
