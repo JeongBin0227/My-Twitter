142d60ec5797447e79a3cfbf133a1a92e1f8de47
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-10-16 02:55:48 +0900

    feat: 구글 및 깃헙 로그인

--- nwitter/src/fbase.js
+++ nwitter/src/fbase.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase/app"
 import "firebase/auth"
 
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -13,4 +13,5 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
+export const firebaseInstance = firebase
 export const authService = firebase.auth()
