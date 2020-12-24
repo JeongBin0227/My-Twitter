e483c8e3b1c832e5f912575ca68b311db7ecf2da
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-12-24 23:22:43 +0900

    feat: Preview Images

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -4,6 +4,7 @@ import Nweet from "components/Nweet"
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("")
   const [nweets, setNweets] = useState([])
+  const [attachment, setAttachment] = useState()
   const getNweets = async () => {
     const dbNweets = await dbService.collection("nweets").get()
     dbNweets.forEach((document) => {
@@ -42,10 +43,14 @@ const Home = ({ userObj }) => {
     const theFile = files[0]
     const reader = new FileReader()
     reader.onloadend = (finishedEvent) => {
-      
+      const {
+        currentTargt : {result}
+      } = finishedEvent
+      setAttachment(result)
     }
     reader.readAsDataURL(theFile)
   }
+  const onClearAttachment = () => setAttachment(null)
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -57,8 +62,12 @@ const Home = ({ userObj }) => {
           maxLength={120}
         />
         <input type="file" accept="image/*" onChange={onFileChange}/>
-
         <input type="submit" value="Nweet" />
+        { attachment && 
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>}
       </form>
       <div>
         {nweets.map((nweet) => (
