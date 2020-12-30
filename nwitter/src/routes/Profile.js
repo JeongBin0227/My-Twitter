ba4c3c084d4fe7571bf6c6e5028eb08372ebdef2
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-12-30 23:38:40 +0900

    feat: Get My Own Nweets

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { auth } from "firebase"
-import { authService } from "fbase"
+import { authService, dbService } from "fbase"
 import { useHistory } from "react-router-dom"
 
 const Profile = () => {
@@ -9,7 +9,13 @@ const Profile = () => {
     authService.signOut()
     history.push("/")
   }
+  const getMyNweets = async () => {
+    const nweets = await dbService.collection("nweets").where("creatorId", "==", userObj.uid).orderBy("createdAt").get()
+  }
 
+  useEffect(() => {
+    getMyNweets()
+  },[])
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
