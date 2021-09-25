187df902795e4cd667928936986809e5d84e1a87
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-09-25 23:48:36 +0900

    feat: Login Form part one

--- nwitter_training/src/routes/Auth.js
+++ nwitter_training/src/routes/Auth.js
@@ -1,4 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.name);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={email}
+          placeholde="Email"
+          required
+          onChange={onChange}
+        />
+        <input
+          type="text"
+          value={password}
+          placeholde="Password"
+          required
+          onChange={onChange}
+        />
+        <input type="text" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
