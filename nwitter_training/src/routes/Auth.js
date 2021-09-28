28df856b9dcd8eb814ce284a7a450b73f5faf632
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-09-28 23:44:56 +0900

    feat: Social Login

--- nwitter_training/src/routes/Auth.js
+++ nwitter_training/src/routes/Auth.js
@@ -38,6 +38,19 @@ const Auth = () => {
   const toggleAccount = () => {
     setNewAccount((prev) => !prev);
   };
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+
+    await authService.signInWithPopup(provider);
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -65,8 +78,12 @@ const Auth = () => {
         {newAccount ? "Sign in" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
