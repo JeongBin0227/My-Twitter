2cad7204323de3415b04bee5a2ca020931357608
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-10-01 01:15:08 +0900

    feat: Nweeting

--- nwitter_training/src/fbase.js
+++ nwitter_training/src/fbase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/database";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqBYb2dIfEf2e1TTMGTgWw9DnJLCpSl2E",
@@ -16,3 +16,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- nwitter_training/src/routes/Home.js
+++ nwitter_training/src/routes/Home.js
@@ -1,9 +1,16 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
   const onSubmit = (event) => {
     event.preventDefault();
+    dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+    console.log("s");
   };
   const onChange = (event) => {
     const {
