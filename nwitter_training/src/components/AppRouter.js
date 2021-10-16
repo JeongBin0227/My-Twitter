19084a27d89bda922302119c0d3237f2a0f63e58
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-10-17 06:03:18 +0900

    feat: Update Profile

--- nwitter_training/src/components/AppRouter.js
+++ nwitter_training/src/components/AppRouter.js
@@ -13,7 +13,7 @@ import Navigation from "./Navigatoin";
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj}/>}
       <Switch>
         {isLoggedIn ? (
           <>
--- nwitter_training/src/routes/Profile.js
+++ nwitter_training/src/routes/Profile.js
@@ -1,13 +1,30 @@
 import { authService, dbService } from 'fbase';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
   };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
   const getMyNweets = async () => {
     const nweets = await dbService
       .collection('nweets')
@@ -22,6 +39,15 @@ const Profile = ({ userObj }) => {
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
