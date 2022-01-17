3a81850b0ee486d7fcfc653ec5893bb3144690de
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-01-18 00:38:51 +0900

    feat: approuter 개발

--- nwitter_training/src/components/AppRouter.js
+++ nwitter_training/src/components/AppRouter.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 import {
   HashRouter as Router,
   Redirect,
   Route,
   Switch,
-} from "react-router-dom";
-import Profile from "routes/Profile";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
-import Navigation from "./Navigatoin";
+} from 'react-router-dom';
+import Profile from 'routes/Profile';
+import Auth from '../routes/Auth';
+import Home from '../routes/Home';
+import Navigation from './Navigatoin';
 
-const AppRouter = ({ isLoggedIn, userObj }) => {
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation userObj={userObj}/>}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
           <>
@@ -21,7 +21,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} refreshUser={refreshUser} />
             </Route>
             <Redirect from="*" to="/"></Redirect>
           </>
